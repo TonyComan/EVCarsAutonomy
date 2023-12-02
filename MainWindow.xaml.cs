b1fee30493f27be416b8fe1e19a5b46444a2f864
Tony Coman <tonycoman87@gamil.com>
2023-12-02 23:50:49 +0200

    Noi Modificari < 23:50

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,14 +20,59 @@ namespace EVCarsAutonomy
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        //Buton minimizare dreapta sus
         private void MinimizeButton_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
+        }
+        
+        //Eveniment pentru Home
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            AfisareMain.Content = null;
+            trymyappTxt.Visibility = Visibility.Collapsed;
+            masinutaPoza.Visibility = Visibility.Collapsed;
+            textjosdreapta.Visibility = Visibility.Collapsed;
+            HomePage homepage = new HomePage();
+
+            AfisareMain.Content = homepage;
+        }
+
+        //Eveniment pentru Analyse
+        private void AnalyseClickBtn(object sender, RoutedEventArgs e)
+        {
+            AfisareMain.Content = null;
+            trymyappTxt.Visibility = Visibility.Collapsed;
+            masinutaPoza.Visibility = Visibility.Collapsed;
+            textjosdreapta.Visibility = Visibility.Collapsed;
+            AnalyzePage analisepage = new AnalyzePage();
+
+            AfisareMain.Content = analisepage;
+        }
+
+        //Eveniment pentru Find
+        private void FindClickBtn(object sender, RoutedEventArgs e)
+        {
+            AfisareMain.Content = null;
+            trymyappTxt.Visibility = Visibility.Collapsed;
+            masinutaPoza.Visibility = Visibility.Collapsed;
+            textjosdreapta.Visibility = Visibility.Collapsed;
+            FindPage findPage = new FindPage();
+
+            AfisareMain.Content = findPage;
+        }
+
+        //Eveniment Pentru Exit
+        private void ExitClickBtn(object sender, RoutedEventArgs e)
+        {
+           Application.Current.Shutdown();
+
         }
     }
 }
