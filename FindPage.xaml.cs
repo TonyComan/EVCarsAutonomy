71756ef47ddcbb8ff99e05c19f1acd65633e4e04
Tony Coman <tonycoman87@gamil.com>
2023-12-04 20:04:01 +0200

    Modificari noi 20:03

--- FindPage.xaml.cs
+++ FindPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,26 @@ namespace EVCarsAutonomy
         public FindPage()
         {
             InitializeComponent();
+
+            
+        }
+
+       
+        private void textboxMasinaMarca_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            // validarea cu Regex 
+            TextBox textBox = (TextBox)sender;
+            string text = textBox.Text;
+
+            if (!Regex.IsMatch(text, "^[a-zA-Z0-9\\s]*$"))
+            {
+                // Interzice caracterele nedorite și afișează un mesaj de eroare
+                textBox.Text = Regex.Replace(text, "[^a-zA-Z0-9\\s]", "");
+                MessageBox.Show("Introdu Corect Numele si Modelul");
+            }
+
+
         }
     }
 }
